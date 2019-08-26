61a42236896caa23d36ed05c27f0593fdf258682
Duy <duytq@dgroup.co>
2019-08-26 23:57:46 +0700

    create the first react component and add webpack-dev-server

--- webpack.development.config.js
+++ webpack.development.config.js
@@ -1,14 +1,24 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
+        publicPath: '/',
         path: path.resolve(__dirname, 'dist')
     },
     module: {
         rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: "babel-loader"
+                }
+            },
             {
                 test: /\.css$/,
                 use: [
@@ -19,6 +29,14 @@ module.exports = {
         ]
     },
     plugins: [
-        new HtmlWebpackPlugin()
-    ]
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './index.html')
+        })
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000
+    },
 }
