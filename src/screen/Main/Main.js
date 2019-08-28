86665ae03417d75734a7b02a5ffec3a553ffef3a
Duy <duytq@dgroup.co>
2019-08-28 17:25:08 +0700

    add indicator for chunk

--- src/screen/Main/Main.js
+++ src/screen/Main/Main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { splitMessageChunk } from '../../services/MessageServices';
 
-const a = splitMessageChunk("I can't believe Tweeter now supports chunking my messages, so I don't have to do it myself.");
+const a = splitMessageChunk("I can't believe Tweeter now supports chunking my messages, so I don't have to do it myself. I can't believe Tweeter now supports chunking my messages, so I don't have to do it myself. I can't believe Tweeter now supports chunking my messages, so I don't have to do it myself. I can't believe Tweeter now supports chunking my messages, so I don't have to do it myself. I can't believe Tweeter now supports chunking my messages, so I don't have to do it myself. I can't believe Tweeter now supports chunking my messages, so I don't have to do it myself. I can't believe Tweeter now supports chunking my messages, so I don't have to do it myself. I can't believe Tweeter now supports chunking my messages, so I don't have to do it myself. I can't believe Tweeter now supports chunking my messages, so I don't have to do it myself. I can't believe Tweeter now supports chunking my messages, so I don't have to do it myself. I can't believe Tweeter now supports chunking my messages, so I don't have to do it myself.");
 console.log(a);
 class Main extends React.Component {
     render() {
--- src/services/MessageServices.js
+++ src/services/MessageServices.js
@@ -2,11 +2,6 @@ const MAX_LENGTH_CHUNK = 50;
 const LENGTH_OF_BACK_SLASH = 1;
 const LENGTH_OF_SPACE = 1;
 
-const splitMessage = function (msg) {
-    const msgArray = msg.trim().split(" ");
-    return msgArray;
-}
-
 const msgLengthChunk = function (msg) {
     const MAX_LENGTH_DATA_CHUNK = MAX_LENGTH_CHUNK - LENGTH_OF_BACK_SLASH - LENGTH_OF_SPACE;
     for (let i = 1; ;i++) {
@@ -16,9 +11,6 @@ const msgLengthChunk = function (msg) {
         if (pivot > 0) {
             return [msgLengthForEachChunk, i];
         }
-        if (i> 100) {
-            break;
-        }
     }
 }
 
@@ -26,7 +18,26 @@ export const splitMessageChunk = function (msg) {
     if (msg.length <= MAX_LENGTH_CHUNK) {
         return [msg]; 
     }
-    const msgArray = splitMessage(msg);
-    const [msgLengthForEachChunk, totalChunk] = msgLengthChunk(msg);
-    console.log(msgLengthForEachChunk, totalChunk);
+    let [msgLengthForEachChunk, totalChunk] = msgLengthChunk(msg);
+    const msgWithoutPartIndicator = [];
+    let currentString = '';
+    for (let i = 0; i < msg.length; i++) {
+        if (currentString.length === 0) {
+            currentString += msg[i].trim();
+        } else {
+            currentString += msg[i];
+        }
+        if (i > msgLengthForEachChunk) {
+            msgWithoutPartIndicator.push(currentString);
+            currentString = '';
+            msgLengthForEachChunk *= 2;
+        }
+        if (i === msg.length - 1 && currentString.length > 0) {
+            msgWithoutPartIndicator.push(currentString);
+        }
+    }
+    for (let i = 0; i < msgWithoutPartIndicator.length; i++) {
+        msgWithoutPartIndicator[i] = `${i+1}/${msgWithoutPartIndicator.length} ${msgWithoutPartIndicator[i]}`;
+    }
+    return msgWithoutPartIndicator
 }
